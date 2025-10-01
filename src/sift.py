9d5ce3beaf83afab0bee263788ae093e5543340a
Emma <emma.rechon@live.fr>
2025-10-01 13:04:59 +0200

    Rectifiction of sift code : choose only the most importants points

--- src/sift.py
+++ src/sift.py
@@ -1,14 +1,16 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('data/images/city_hall/city_hall_1.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('data/images/city_hall/city_hall1.jpg', cv2.IMREAD_GRAYSCALE)
 sift = cv2.SIFT_create()
 if img is None:
     raise FileNotFoundError("L’image n’a pas été trouvée ou n’a pas pu être chargée.")
 else : 
     keypoints, descriptors = sift.detectAndCompute(img, None)
 
-    img_with_keypoints = cv2.drawKeypoints(img, keypoints, None, color=(0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    filtered_keypoints = [kp for kp in keypoints if kp.response > 0.05]
+    print(len(filtered_keypoints))
+    img_with_keypoints = cv2.drawKeypoints(img, filtered_keypoints, None, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Affichage avec matplotlib
     plt.figure(figsize=(10, 8))
@@ -17,14 +19,16 @@ else :
     plt.axis('off')
     plt.show()
 
-img = cv2.imread('data/images/city_hall/city_hall_2.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('data/images/city_hall/city_hall2.jpg', cv2.IMREAD_GRAYSCALE)
 sift = cv2.SIFT_create()
 if img is None:
     raise FileNotFoundError("L’image n’a pas été trouvée ou n’a pas pu être chargée.")
 else :
     keypoints, descriptors = sift.detectAndCompute(img, None)
 
-    img_with_keypoints = cv2.drawKeypoints(img, keypoints, None, color=(0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    filtered_keypoints = [kp for kp in keypoints if kp.response > 0.05]
+    print(len(filtered_keypoints))
+    img_with_keypoints = cv2.drawKeypoints(img, filtered_keypoints, None, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Affichage avec matplotlib
     plt.figure(figsize=(10, 8))
